cba556a179e410ddeabfce8df7c815b19e9f290b
jorge gomez <jorge.gomez@tumesadeayuda.com>
2019-10-30 09:35:37 -0500

    splitting url with regex groups

--- regex/Program.cs
+++ regex/Program.cs
@@ -7,18 +7,40 @@ namespace regex
     {
         static void Main(string[] args)
         {
-            string pattern = @"(\w*\,?\s?)";
-            Console.WriteLine("Enter a sentece");
-            string sentece = Console.ReadLine();
-            var words = Regex.Split(sentece, pattern);
-            foreach (var word in words)
-            {
-                if (!string.IsNullOrEmpty(word))
-                {
-                    Console.WriteLine(word);
-                }
-            }
+            string pattern = @"(https?):\/\/(\w.*)\/(\w.*)?";
+            
+            string sentece = "https://en.wikipedia.org/wiki";
+            
+            MatchCollection results = Regex.Matches(sentece, pattern);
 
+            PrintURLMatches(results);
+
+            Console.WriteLine("-----------------------------------------------");
+
+            sentece = "http://en.wikipedia.org/wiki/Theoretical_computer_science";
+
+            results = Regex.Matches(sentece, pattern);
+
+            PrintURLMatches(results);
+
+            Console.WriteLine("-----------------------------------------------");
+
+            sentece = "http://en.wikipedia.org/";
+
+            results = Regex.Matches(sentece, pattern);
+
+            PrintURLMatches(results);
+
+        }
+
+        private static void PrintURLMatches(MatchCollection matches)
+        {
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Protocol => {match.Groups[1].Value}\n" +
+                    $"Base URL=> {match.Groups[2].Value}\n" +
+                    $"Query String => {match.Groups[3]?.Value}");
+            }
         }
     }
 }
