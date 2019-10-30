fad415b1367b2997ef0a9acf097c3bf178b86b9a
jorge gomez <jorge.gomez@tumesadeayuda.com>
2019-10-30 08:45:07 -0500

    Add project files.

---
+++ regex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\w*\,?\s?)";
+            Console.WriteLine("Enter a sentece");
+            string sentece = Console.ReadLine();
+            var words = Regex.Split(sentece, pattern);
+            foreach (var word in words)
+            {
+                if (!string.IsNullOrEmpty(word))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+
+        }
+    }
+}
